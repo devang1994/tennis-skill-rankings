afb976a5675ed3a5ef616e850d3aeaf3f7d3dfd3
yuzisee <yuzisee@localhost>
2011-03-15 15:47:10 -0700

    [dev-matlab] Fix syntax error in printout

--- code/run.m
+++ code/run.m
@@ -23,10 +23,10 @@ epsilon_BradleyTerry,
 disp('');
 disp('1pt			3pt		OFFENSIVE PLAYER');
 for n=1:length(names_offense)
-	disp([num2str(readable_Theta_BradleyTerry(:,n)) '		' names_offense{n}]);
+	disp([num2str(readable_Theta_BradleyTerry(n,:)) '		' names_offense{n}]);
 end
 disp('');
 disp('1pt			3pt		DEFENSIVE PLAYER');
 for n=1:length(names_defense)
-	disp([num2str(readable_Theta_BradleyTerry(:,length(names_offense)+n)) '		' names_defense{n}]);
+	disp([num2str(readable_Theta_BradleyTerry(length(names_offense)+n,:)) '		' names_defense{n}]);
 end
