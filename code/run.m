2eabf2b86c7fe8e8a7872503eac4e1b95a01414c
jdhuang <jdhuang@localhost>
2011-03-15 04:40:22 -0700

    Bug has been identified. There is no need to fit a sigma, roughly for the same reason that there is no need to fit an intercept term

--- code/run.m
+++ code/run.m
@@ -1,8 +1,12 @@
 % After running:
 %   $python ../data/build_dataset.py ../data/raw/20070204.DETCLE.csv
+clear all
+
 loaddata
 
 MAX_ITER = 500
 
+dbclear all
 [Theta_BradleyTerry loglikelihood_BradleyTerry] = basketball_network_EM(dataset, MAX_ITER, false)
+dbclear all
 [Theta_ThurstoneCaseV loglikelihood_ThurstoneCaseV] = basketball_network_EM(dataset, MAX_ITER, true)
