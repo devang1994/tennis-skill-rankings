b7d2fc038be05f0bf390efb769481f55613e1547
yuzisee <yuzisee@localhost>
2011-03-15 02:21:21 -0700

    Move some files around

---
+++ code/run.m
@@ -0,0 +1,8 @@
+% After running:
+%   $python ../data/build_dataset.py ../data/raw/20070204.DETCLE.csv
+loaddata.m
+
+MAX_ITER = 500
+gaussian = false
+
+[Theta ll] = basketball_network_EM(dataset, MAX_ITER, gaussian)
