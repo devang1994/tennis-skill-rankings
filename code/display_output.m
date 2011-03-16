dbcd95f4b5b338124e0e6d1524c5b2913fa3cb80
yuzisee <yuzisee@localhost>
2011-03-15 18:59:31 -0700

    [main] Show M_count in display_output.m

--- code/display_output.m
+++ code/display_output.m
@@ -1,4 +1,6 @@
 
+M_count = sum(bsxfun(@eq,dataset(:,1),0:3))',
+
 if exist('Theta_BradleyTerry','var')
 	% The Gaussian we use during probit has variance 10 since it is assumed to be the sum of 10 independent unit gaussian player performances.
 	% A logit with the same variance would require rescaling the theta values by: sigma^2 = pi^2 s^2 / 3 ==> s = sqrt(3) sigma / pi
