bbffaaba7a15d5cb04809aa73bc669ad70c0e634
yuzisee <yuzisee@localhost>
2011-03-15 21:25:54 -0700

    [dev-matlab] Performance refactoring, printout refactoring, this particular commit should have no functional changes

--- code/run.m
+++ code/run.m
@@ -12,4 +12,13 @@ MAX_ITER = 500
 [Theta_ThurstoneCaseV loglikelihood_ThurstoneCaseV epsilon_ThurstoneCaseV] = basketball_network_EM(dataset, MAX_ITER, true)
 
 
-display_output
+% PRINT OUTPUT
+M_count = sum(bsxfun(@eq,dataset(:,1),0:3))',
+
+if exist('Theta_BradleyTerry','var')
+	display_output(Theta_BradleyTerry, loglikelihood_BradleyTerry, epsilon_BradleyTerry, 'Bradley-Terry (logit):', false)
+end
+
+if exist('Theta_ThurstoneCaseV','var')
+	display_output(Theta_ThurstoneCaseV, loglikelihood_ThurstoneCaseV, epsilon_ThurstoneCaseV, 'Thurstone Case V (Hessian probit):', true)
+end
