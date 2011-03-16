0aedfe9ced1743163e324c910181ecaa1654bef6
yuzisee <yuzisee@localhost>
2011-03-15 19:08:05 -0700

    [main] Latest results, now running hessian probit on M=227

--- code/display_output.m
+++ code/display_output.m
@@ -41,12 +41,12 @@ if exist('Theta_ThurstoneCaseV','var')
 	disp('');
 	disp('1pt			3pt		OFFENSIVE PLAYER');
 	for n=1:length(names_offense)
-		disp([num2str(readable_Theta_BradleyTerry(n,:)) '		' names_offense{n}]);
+		disp([num2str(readable_Theta_ThurstoneCaseV(n,:)) '		' names_offense{n}]);
 	end
 	disp('');
 	disp('1pt			3pt		DEFENSIVE PLAYER');
 	for n=1:length(names_defense)
-		disp([num2str(readable_Theta_BradleyTerry(length(names_offense)+n,:)) '		' names_defense{n}]);
+		disp([num2str(readable_Theta_ThurstoneCaseV(length(names_offense)+n,:)) '		' names_defense{n}]);
 	end
 end
 
