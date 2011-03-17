c58dd4f40755ca35cde895f079e95c02da17caa3
yuzisee <yuzisee@localhost>
2011-03-16 18:46:46 -0700

    [results] Compare initialization results using the same code, write more of the report

--- code/display_output_all.m
+++ code/display_output_all.m
@@ -3,18 +3,18 @@
 M_count = sum(bsxfun(@eq,dataset(:,1),0:3))',
 
 if exist('BradleyTerry_parameters','var')
-	display_output(player_names, BradleyTerry_parameters, 'Bradley-Terry (logit) parameter initialization:', sum(M_count), false)
+	display_output(player_names, BradleyTerry_parameters, 'Bradley-Terry (logit) parameter initialization:', sum(M_count), false);
 end
 
 if exist('ThurstoneCaseV_parameters','var')
-	display_output(player_names, ThurstoneCaseV_parameters, 'Thurstone Case V (Hessian probit) parameter initialization:', sum(M_count), true)
+	display_output(player_names, ThurstoneCaseV_parameters, 'Thurstone Case V (probit) parameter initialization:', sum(M_count), true);
 end
 
-if exist('BradleyTerry_softassignment','var')
-	display_output(player_names, BradleyTerry_softassignment, 'Bradley-Terry (logit) soft-assignment initialization:', sum(M_count), false)
+if exist('BradleyTerry_softassignments','var')
+	display_output(player_names, BradleyTerry_softassignments, 'Bradley-Terry (logit) soft-assignment initialization:', sum(M_count), false);
 end
 
-if exist('ThurstoneCaseV_softassignment','var')
-	display_output(player_names, ThurstoneCaseV_softassignment, 'Thurstone Case V (Hessian probit) soft-assignment initialization:', sum(M_count), true)
+if exist('ThurstoneCaseV_softassignments','var')
+	display_output(player_names, ThurstoneCaseV_softassignments, 'Thurstone Case V (probit) soft-assignment initialization:', sum(M_count), true);
 end
 
