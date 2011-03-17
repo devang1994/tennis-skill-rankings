ec524695117c928174d28a8534f8f8cdd8dd31a7
yuzisee <yuzisee@localhost>
2011-03-16 19:58:29 -0700

    [dev-threeteam] Create the data for three-team test case

---
+++ code/display_output_all.m
@@ -0,0 +1,32 @@
+
+% PRINT OUTPUT
+M_count = sum(bsxfun(@eq,dataset(:,1),0:3))',
+
+players = struct('M', sum(M_count), 'possessions', sum(dataset(:,2:end))');
+players.names = player_names;
+
+disp('Table of Contents')
+disp('  LOGISTIC/logit')
+disp('    Choose E_D soft-assignment, start with E-step')
+disp('    Choose parameter values, start with M-step')
+disp('  GAUSSIAN/probit')
+disp('    Choose E_D soft-assignment, start with E-step')
+disp('    Choose parameter values, start with M-step')
+
+
+if exist('BradleyTerry_softassignments','var')
+	display_output(players, BradleyTerry_softassignments, 'Bradley-Terry (logit) soft-assignment initialization:', false);
+end
+if exist('BradleyTerry_parameters','var')
+	display_output(players, BradleyTerry_parameters, 'Bradley-Terry (logit) parameter initialization:', false);
+end
+
+if exist('ThurstoneCaseV_softassignments','var')
+	display_output(players, ThurstoneCaseV_softassignments, 'Thurstone Case V (probit) soft-assignment initialization:', true);
+end
+if exist('ThurstoneCaseV_parameters','var')
+	display_output(players, ThurstoneCaseV_parameters, 'Thurstone Case V (probit) parameter initialization:', true);
+end
+
+
+
