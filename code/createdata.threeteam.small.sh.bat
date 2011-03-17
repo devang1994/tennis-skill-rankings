f43ed06dd18ca184224bf882a5a50f8b35035398
yuzisee <yuzisee@localhost>
2011-03-16 22:14:50 -0700

    [dev-three-team] Rewrite soft_assignments so it matches test_train.m

--- code/createdata.threeteam.small.sh.bat
+++ code/createdata.threeteam.small.sh.bat
@@ -1,10 +1,10 @@
 mkdir threeteam.training
 pushd threeteam.training
-python ../../data/build_dataset.py ../../data/raw/threeteam/20090311.BOSMIA.csv ../../data/raw/threeteam/20090330.ORLMIA.csv
+python ../../data/build_dataset.py ../../data/raw/threeteam/20090311.BOSMIA.csv ../../data/raw/threeteam/20090410.MIABOS.csv ../../data/raw/threeteam/20090330.ORLMIA.csv
 python -c "import os;os.rename('loaddata.m','loaddata_train.m')"
 popd
 mkdir threeteam.test
 pushd threeteam.test
-python ../../data/build_dataset.py ../../data/raw/threeteam/20090325.BOSORL.csv
+python ../../data/build_dataset.py ../../data/raw/threeteam/20090308.ORLBOS.csv
 python -c "import os;os.rename('loaddata.m','loaddata_test.m')"
 popd
