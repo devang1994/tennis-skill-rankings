ecc1465e6cd2216ae3e671297b4a36483e5bb985
yuzisee <yuzisee@localhost>
2011-03-16 17:22:36 -0700

    [dev-initialization] Implementing different initialization options

--- code/display_output_all.m
+++ code/display_output_all.m
@@ -2,10 +2,19 @@
 % PRINT OUTPUT
 M_count = sum(bsxfun(@eq,dataset(:,1),0:3))',
 
-if exist('Theta_BradleyTerry','var')
-	display_output(player_names, Theta_BradleyTerry, loglikelihood_BradleyTerry, epsilon_BradleyTerry, 'Bradley-Terry (logit):', sum(M_count), false)
+if exist('Theta_BradleyTerry_parameters','var')
+	display_output(player_names, Theta_BradleyTerry, loglikelihood_BradleyTerry, epsilon_BradleyTerry, 'Bradley-Terry (logit) parameter initialization:', sum(M_count), false)
 end
 
-if exist('Theta_ThurstoneCaseV','var')
-	display_output(player_names, Theta_ThurstoneCaseV, loglikelihood_ThurstoneCaseV, epsilon_ThurstoneCaseV, 'Thurstone Case V (Hessian probit):', sum(M_count), true)
+if exist('Theta_ThurstoneCaseV_parameters','var')
+	display_output(player_names, Theta_ThurstoneCaseV, loglikelihood_ThurstoneCaseV, epsilon_ThurstoneCaseV, 'Thurstone Case V (Hessian probit) parameter initialization:', sum(M_count), true)
 end
+
+if exist('Theta_BradleyTerry_softassignment','var')
+	display_output(player_names, Theta_BradleyTerry, loglikelihood_BradleyTerry, epsilon_BradleyTerry, 'Bradley-Terry (logit) soft-assignment initialization:', sum(M_count), false)
+end
+
+if exist('Theta_ThurstoneCaseV_softassignment','var')
+	display_output(player_names, Theta_ThurstoneCaseV, loglikelihood_ThurstoneCaseV, epsilon_ThurstoneCaseV, 'Thurstone Case V (Hessian probit) soft-assignment initialization:', sum(M_count), true)
+end
+
